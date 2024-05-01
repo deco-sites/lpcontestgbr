2ac9ad18c95927b071a141f8e9ca7254044883cc
GabrielTenorio-social <g.tenorio@socialsa.com>
2024-05-01 19:07:30 -0300

    feat: Services section

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -5,24 +5,26 @@
 import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
 import * as $$$$$$0 from "./sections/Content/Bio.tsx";
-import * as $$$$$$1 from "./sections/Footer.tsx";
-import * as $$$$$$2 from "./sections/Header/FixedMenu.tsx";
-import * as $$$$$$3 from "./sections/Hero/Hero.tsx";
-import * as $$$$$$4 from "./sections/Image/EaselGallery.tsx";
-import * as $$$$$$5 from "./sections/Image/Slider.tsx";
-import * as $$$$$$6 from "./sections/ItWorks.tsx";
-import * as $$$$$$7 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$1 from "./sections/Content/Services.tsx";
+import * as $$$$$$2 from "./sections/Footer.tsx";
+import * as $$$$$$3 from "./sections/Header/FixedMenu.tsx";
+import * as $$$$$$4 from "./sections/Hero/Hero.tsx";
+import * as $$$$$$5 from "./sections/Image/EaselGallery.tsx";
+import * as $$$$$$6 from "./sections/Image/Slider.tsx";
+import * as $$$$$$7 from "./sections/ItWorks.tsx";
+import * as $$$$$$8 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
     "site/sections/Content/Bio.tsx": $$$$$$0,
-    "site/sections/Footer.tsx": $$$$$$1,
-    "site/sections/Header/FixedMenu.tsx": $$$$$$2,
-    "site/sections/Hero/Hero.tsx": $$$$$$3,
-    "site/sections/Image/EaselGallery.tsx": $$$$$$4,
-    "site/sections/Image/Slider.tsx": $$$$$$5,
-    "site/sections/ItWorks.tsx": $$$$$$6,
-    "site/sections/Theme/Theme.tsx": $$$$$$7,
+    "site/sections/Content/Services.tsx": $$$$$$1,
+    "site/sections/Footer.tsx": $$$$$$2,
+    "site/sections/Header/FixedMenu.tsx": $$$$$$3,
+    "site/sections/Hero/Hero.tsx": $$$$$$4,
+    "site/sections/Image/EaselGallery.tsx": $$$$$$5,
+    "site/sections/Image/Slider.tsx": $$$$$$6,
+    "site/sections/ItWorks.tsx": $$$$$$7,
+    "site/sections/Theme/Theme.tsx": $$$$$$8,
   },
   "apps": {
     "site/apps/decohub.ts": $$$$$$$$$$$0,
