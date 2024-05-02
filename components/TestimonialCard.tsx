0eb8c13f09ede8d06b3a4d4b1d74720874efa091
Gabriel Tenório <g.tenorio@socialsa.com>
2024-05-02 08:35:44 -0300

    feat: testimonials

---
+++ components/TestimonialCard.tsx
@@ -0,0 +1,35 @@
+import Image from "apps/website/components/Image.tsx";
+import { ImageWidget } from "apps/admin/widgets.ts";
+
+export interface TestimonialCardProps {
+    image: {
+        src: ImageWidget
+        /**@default Gabriel's art */
+        alt?: string
+    }
+   /**@format textarea */
+    content: string
+    autor: string
+}
+
+function TestimonialCard({ image, autor, content}: TestimonialCardProps) {
+    return (
+        <div class="flex flex-col items-center gap-11 px-4 py-6 rounded-md min-w-56 max-w-56 border border-theme-light-beige bg-theme-white">
+            <div class="rounded-full w-28 h-28 overflow-hidden flex justify-center items-center">
+                <Image
+                    src={image.src}
+                    alt={image.alt}
+                    width={122}
+                />
+            </div>
+
+            <p class="font-light text-sm text-theme-dark-brown">
+                {content}
+            </p>
+
+            <span class="font-light text-xs text-slate-400">{autor}</span>
+        </div>
+    )
+}
+
+export default TestimonialCard
--- sections/Content/Testimonials.tsx
+++ sections/Content/Testimonials.tsx
@@ -1,6 +1,29 @@
-function Testimonials() {
+import TestimonialCard, { TestimonialCardProps } from "site/components/TestimonialCard.tsx";
+
+export interface Props {
+    title: string
+    subtitle?: string
+    testimonialCards: TestimonialCardProps[]
+}
+
+function Testimonials({ title, subtitle, testimonialCards }: Props) {
     return (
-        <h1>Testimonials</h1>
+        <section class="py-16 px-2 flex flex-col gap-8 bg-theme-gray">
+            <div class="flex flex-col gap-4 text-center">
+                <h2 class="text-theme-light-brown font-serif text-4xl">
+                    {title}
+                </h2>
+                <p class="text-theme-light-brown text-lg font-light">
+                    {subtitle}
+                </p>
+            </div>
+
+            <div class="flex gap-6 overflow-x-scroll">
+                {testimonialCards && testimonialCards.map(card => (
+                    <TestimonialCard autor={card.autor} content={card.content} image={card.image} />
+                ))}
+            </div>
+        </section>
     )
 }
 
