c4d59b22dd8d8c00ccd026d06cb3d5f513c196d6
GabrielTenorio-social <g.tenorio@socialsa.com>
2024-05-02 20:45:57 -0300

    feat: GetInTouch

--- sections/GetInTouch/GetInTouch.tsx
+++ sections/GetInTouch/GetInTouch.tsx
@@ -1,4 +1,5 @@
 import { AvailableIcons } from "site/components/ui/Icon.tsx";
+import Icon from "site/components/ui/Icon.tsx";
 
 interface ContactOption {
   /**
@@ -15,8 +16,31 @@ export interface Props {
   options?: ContactOption[]
 }
 
-function GetInTouch({ title, options, subtitle }: Props) {
-  return <h1>GetInTouch</h1>
+function GetInTouch({ title, subtitle, options }: Props) {
+  return (
+    <section class="flex flex-col justify-center items-center px-2 py-9 bg-theme-offwhite">
+      <div class="flex flex-col gap-6 border border-theme-gray px-2 py-9">
+
+        <h2 class="font-serif text-4xl text-theme-dark-brown text-center">
+          {title}
+        </h2>
+
+        <p class="text-2xl text-theme-light-brown font-light text-center">
+          {subtitle}
+        </p>
+
+        <ul class="flex flex-col gap-4 pl-5">
+          {options && options.map(option => (
+            <li class="flex gap-3 text-theme-dark-brown font-light">
+              <Icon id={option.icon} size={28} />
+              <span>{option.text}</span>
+            </li>
+          ))}
+        </ul>
+
+      </div>
+    </section>
+  )
 }
 
 export default GetInTouch
